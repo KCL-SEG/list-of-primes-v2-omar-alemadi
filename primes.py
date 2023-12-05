824a6320ec1fd23c2a3c1672cecc04224aefa6b3
Omar Al-Emadi <k21043327@kcl.ac.uk>
2023-12-05 15:20:11 +0000

    pushed code

--- primes.py
+++ primes.py
@@ -2,5 +2,23 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError("must be a positive number")
+
+    prime_list = []
+    x = 2
+
+    while len(prime_list) < number_of_primes:
+        is_prime = all(x % prime != 0 for prime in prime_list)
+        if is_prime:
+            prime_list.append(x)
+        x += 1
+
+    return prime_list
+
+# Example usage:
+try:
+    result = primes(10)
+    print(result)
+except ValueError as e:
+    print(f"Error: {e}")
