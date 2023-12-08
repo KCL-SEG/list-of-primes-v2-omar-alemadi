845ae23abb7896c61c29ae29c3ccc34890137d09
Omar Al-Emadi <k21043327@kcl.ac.uk>
2023-12-08 04:19:44 +0000

    pushh

--- primes.py
+++ primes.py
@@ -3,19 +3,23 @@
 
 def primes(number_of_primes):
     if number_of_primes <= 0:
-        print("Error: must be a positive number")
-        return []
+        raise ValueError("must be apositive number")
 
     list = []
-    x = 2
+    num = 2
 
     while len(list) < number_of_primes:
-        if all(x % prime != 0 for prime in list):
-            list.append(x)
-        x += 1
+        isp = all(num % prime != 0 for prime in list)
+        if isp:
+            list.append(num)
+        num += 1
 
     return list
 
 # Example usage:
-result = primes(10)
-print(result)
+try:
+    result = primes(10)
+    print(result)
+except ValueError as e:
+    print(f"Error: {e}")
+
